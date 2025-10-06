de0e45bf0344ed4ba45797c8453691755aafa626
Yuya Deno <deltagraviton@gmail.com>
2025-10-06 21:38:05 +0900

    add summation and subtraction

--- 9cc.c
+++ 9cc.c
@@ -6,12 +6,31 @@ int main(int argc, char **argv) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
-    int num = atoi(argv[1]);
+    char *code = argv[1];
 
+    // strtol(str, pointer, num) : str を読み込み、pointer を一つずらし、 int に変換する
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", num);
+    printf("    mov rax, %ld\n", strtol(code, &code, 10));   
+
+    while (*code) {     // code が null pointer でないなら
+        if (*code == '+') {
+            code += 1;
+            printf("    add rax, %ld\n", strtol(code, &code, 10));
+            continue;
+        }
+
+        if (*code == '-') {
+            code += 1;
+            printf("    sub rax, %ld\n", strtol(code, &code, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *code);
+        return 1;
+    }
+
     printf("    ret\n");
     return 0;
 }
